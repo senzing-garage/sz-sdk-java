bb17833fa4af9b47780883aaa1303b4aee1e587e
Barry M. Caceres <barrycaceres@icloud.com>
2025-07-11 10:46:34 -0700

    Fixed package for SzEntityIdsTest

--- src/test/java/com/senzing/sdk/SzEntityIdsTest.java
+++ src/test/java/com/senzing/sdk/SzEntityIdsTest.java
@@ -1,4 +1,4 @@
-package com.senzing.sdk.core;
+package com.senzing.sdk;
 
 import java.util.Set;
 import java.util.Iterator;
@@ -15,7 +15,6 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import com.senzing.sdk.SzEntityIds;
 import com.senzing.sdk.test.AbstractTest;
 
 import static org.junit.jupiter.api.TestInstance.Lifecycle;
