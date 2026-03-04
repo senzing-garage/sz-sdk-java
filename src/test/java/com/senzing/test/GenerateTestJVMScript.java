a11376271a3b37a42aeb38420b03a12459751f85
Barry M. Caceres <barrycaceres@icloud.com>
2026-03-03 22:51:20 -0800

    Changed GenerateTestJVMScript to always use /opt/homebrew/lib since this should work on even older Senzing versions that shipped libssl3

--- src/test/java/com/senzing/test/GenerateTestJVMScript.java
+++ src/test/java/com/senzing/test/GenerateTestJVMScript.java
@@ -155,10 +155,7 @@ public class GenerateTestJVMScript {
                     senzingDir = new File("/Applications/Senzing.app/Contents/Resources/app/er");
                 }
                 libDir = new File(senzingDir, "lib");
-                platformLibDir = new File(libDir, "macos");
-                if (!platformLibDir.exists()) {
-                    platformLibDir = new File("/opt/homebrew/lib");
-                }
+                platformLibDir = new File("/opt/homebrew/lib");
                 break;
             case UNIX:
                 if (senzingDir == null) {
